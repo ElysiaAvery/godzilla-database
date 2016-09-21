d3bc0e659d82491ffab94680e1786fdcda132880
Elysia Nason and Sandro Alvarez <elysia+sandro@>
2016-09-21 16:13:03 -0700

    add templates adn forms to add godzillas and corresponding medias

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,44 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      model.put("godzillas", Godzilla.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/godzilla", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      String era = request.queryParams("era");
+      String enemy = request.queryParams("enemy");
+      Godzilla newGodzilla = new Godzilla(era, enemy);
+      newGodzilla.save();
+      model.put("godzillas", Godzilla.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/godzilla/:id/medias", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      Godzilla godzilla = Integer.parseInt(request.params("id"));
+      model.put("medias", Media.all());
+      model.put("godzilla", godzilla);
+      model.put("template", "templates/godzilla.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/godzilla/:id/medias", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      String type = request.queryParams("type");
+      String title = request.queryParams("title");
+      String description = request.queryParams("description");
+      int godzillaId = Integer.parseInt(request.params(":id"));
+      Media media = new Media(type, title, description, godzillaId);
+      media.save();
+      model.put("medias", Godzilla.find(godzillaId).getMedias());
+      model.put("template", "templates/medias.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
